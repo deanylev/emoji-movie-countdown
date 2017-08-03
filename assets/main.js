7c5697f34718f52a50a202974335836084668a43
Dean Levinson <dean.e.levinson@gmail.com>
2017-08-03 10:16:39 +1000

    Add time unit chooser

--- assets/main.js
+++ assets/main.js
@@ -2,11 +2,50 @@ $(document).ready(function() {
 
   var emojis = ['✌️','😂','😝','😁','😱','👉','🙌','🍻','🔥','🌈','☀','🎈','🌹','💄','🎀','⚽','🎾','🏁','😡','👿','🐻','🐶','🐬','🐟','🍀','👀','🚗','🍎','💝','💙','👌','❤️','😍','😉','😓','😳','💪','💩','🍸','🔑','💖','🌟','🎉','🌺','🎶','👠','🏈','⚾','🏆','👽','💀','🐵','🐮','🐩','🐎','💣','👃','👂','🍓','💘','💜','👊','💋','😘','😜','😵','🙏','👋','🚽','💃','💎','🚀','🌙','🎁','⛄','🌊','⛵','🏀','🎱','💰','👶','👸','🐰','🐷','🐍','🐫','🔫','👄','🚲','🍉','💛','💚'];
 
-  $('#countdown').countdown('2017/09/14', function(event) {
+  var date = '2017/09/14';
+
+  $('#countdown-full').countdown(date, function(event) {
 
     var i = Math.floor(emojis.length * Math.random());
 
-    $(this).html(event.strftime('<h1>Will Launch In Australia In:</h1><h3>%D Days<br>%H Hours<br>%M Minutes<br>%S Seconds</h3><h2>' + emojis[i] + '</h2>'));
+    $(this).html(event.strftime('<h3>%D Days<br>%H Hours<br>%M Minutes<br>%S Seconds</h3><h2>' + emojis[i] + '</h2>'));
+
+  });
+
+  $('#countdown-hours').countdown(date, function(event) {
+
+    var i = Math.floor(emojis.length * Math.random());
+
+    $(this).html(event.strftime('<h3>%I Hours</h3><h2>' + emojis[i] + '</h2>'));
+
+  });
+
+  $('#countdown-minutes').countdown(date, function(event) {
+
+    var i = Math.floor(emojis.length * Math.random());
+
+    $(this).html(event.strftime('<h3>%N Minutes</h3><h2>' + emojis[i] + '</h2>'));
+
+  });
+
+  $('#countdown-seconds').countdown(date, function(event) {
+
+    var i = Math.floor(emojis.length * Math.random());
+
+    $(this).html(event.strftime('<h3>%T Seconds</h3><h2>' + emojis[i] + '</h2>'));
+
+  });
+
+  $('input[type="radio"]').each(function() {
+
+    var div = $('#countdown-' + $(this).attr('id'));
+
+    $(this).click(function() {
+
+      $('.countdown').hide();
+      div.show();
+
+    });
 
   });
 
